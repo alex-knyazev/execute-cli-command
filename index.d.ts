afa122d51121fb9e41f2b12b28e3c4270bd2c99f
Alexander Knyazev <alexander.knyazev@progforce.com>
2019-11-07 13:51:28 +0000

    add d.ts file

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare module 'execute-cli' {
+  export default function executeCliCommand(
+    bashText: string,
+    executeCliOptions: {
+      isLookForErrors: boolean;
+      isSplitByEnter: boolean;
+    }
+  ): Promise<{
+    errors: Array<string>;
+    body: Array<string>;
+  }>;
+}
